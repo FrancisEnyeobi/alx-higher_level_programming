2e61d0557ec979ad26b589f1e1c536d952576a58
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2023-07-09 04:06:19 -0700

    Task 0 to 5

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+
 a = 1
 b = 2
 
-if __name__ == "__main__":
-    from add_0 import add
-
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+result = add(a, b)
+print("{} + {} = {}".format(a, b, result))
